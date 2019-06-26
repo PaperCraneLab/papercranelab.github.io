c36bd73569f67a762f571f297441add3b7ef5060
mathuramg <mmg9291@gmail.com>
2019-06-26 12:28:25 +0530

    add files

---
+++ precache-manifest.02c97f23b79d080a6ded7941c7d4a617.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2ffff1f509a4bc02b5a9",
+    "url": "/static/js/main.2ffff1f5.chunk.js"
+  },
+  {
+    "revision": "c4fb699a57b4f5c39546",
+    "url": "/static/js/1.c4fb699a.chunk.js"
+  },
+  {
+    "revision": "8043bf074e267b36350e3bccfbd26293",
+    "url": "/index.html"
+  }
+];
